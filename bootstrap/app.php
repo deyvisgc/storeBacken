fa5042b0fd325a24369f3ea75bc5b6f54df494b5
zannier7 <zanniervargas@upeu.edu.pe>
2021-01-26 11:17:42 -0500

    CONFIG: Configuracion en el archivo app, habilite middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -77,9 +77,9 @@ $app->configure('app');
      'Vluzrmos\LumenCors\CorsMiddleware'
  ]);
 
-// $app->routeMiddleware([
-//     'auth' => App\Http\Middleware\Authenticate::class,
-// ]);
+ $app->routeMiddleware([
+     'auth' => App\Http\Middleware\Authenticate::class,
+ ]);
 
 /*
 |--------------------------------------------------------------------------
@@ -92,9 +92,9 @@ $app->configure('app');
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
+ $app->register(App\Providers\AppServiceProvider::class);
+ $app->register(App\Providers\AuthServiceProvider::class);
+ $app->register(App\Providers\EventServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
